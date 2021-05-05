3444cba3df18728031ff21747108324127cd0757
sahoodex <deepak.sahoo1@ltts.com>
2021-05-05 20:56:04 +0530

    assignment 4

---
+++ assignment4.py
@@ -0,0 +1,16 @@
+#second smallest number in a list
+
+
+def find_second_smallest(a):
+    a.sort()
+    return a[1]
+
+
+
+
+if __name__ == "__main__":
+    a=[]
+    n = int(input("Enter the length of list: "))
+    for i in range(n):
+        a.append(int(input("Enter the element at index "+str(i))))
+    print("The second smallest element is: {}".format(find_second_smallest(a)))
