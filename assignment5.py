d9aacde2d10ea1efd9a5ba424fcabbfe51e52e86
sahoodex <deepak.sahoo1@ltts.com>
2021-05-05 21:37:37 +0530

    assignment 5

---
+++ assignment5.py
@@ -0,0 +1,15 @@
+def jump(a,n):
+    last = a[0]
+    for i in range(n):
+        if(i == n-1):
+            a[i] = last
+        else:
+            a[i] = a[i+1]
+    return a
+
+if __name__ == "__main__":
+    a=[]
+    n = int(input("Enter the length of list: "))
+    for i in range(n):
+        a.append(int(input("Enter the element at index "+str(i))))
+    print("The updated list is: {}".format(jump(a, n)))
